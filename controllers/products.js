025832212d3c13922f331515df54a4da7a8fc241
ivansing <lancedealway@gmail.com>
2023-09-29 11:43:18 -0500

    Controller funtionality request fields, reGex,sort

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,16 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'ab'  
-  const products = await Product.find({
-    name: {$regex:search,$options: 'i'},
-  });
-  res.status(200).json({ products, nbHits: products.length });
+  const products = await Product.find({ price: { $gt: 50 } })
+    .sort("price")
+    .select("name price");
+
+  res.status(200).json({ products, hbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  // API hn.algolia.com/api
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   // if there are features products only show those ones
@@ -20,13 +21,56 @@ const getAllProducts = async (req, res) => {
     queryObject.company = company;
   }
 
-  if(name){
-    queryObject.name = {$regex: name,$options: 'i'};
+  // convert user friendly filters to mongoose properties object
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
   }
-  console.log(queryObject);
 
-  // otherwise show me all the products
-  const products = await Product.find(queryObject);
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
+  //
+  console.log(queryObject);
+  let result = Product.find(queryObject);
+  // sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createAt");
+  }
+
+  // select (field)
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    result = result.select(fieldList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
